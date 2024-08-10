6afb654ac80f12c1ed5fd942e083795ef36055bf
Matt George <mgeorge@hashicorp.com>
2024-08-10 10:28:11 -0600

    more channel coverage

--- internal/destination/channel_test.go
+++ internal/destination/channel_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/shoenig/test/must"
+	"golang.org/x/sync/errgroup"
 
 	optimusv1 "github.com/binarymatt/optimus/gen/optimus/v1"
 )
@@ -13,6 +14,7 @@ func TestChannelSetup(t *testing.T) {
 	c := &ChannelDestination{}
 	must.Nil(t, c.Setup())
 }
+
 func TestChannelClose(t *testing.T) {
 	ch := make(chan *optimusv1.LogEvent)
 	c := &ChannelDestination{
@@ -29,4 +31,22 @@ func TestDeliverMissingChannel(t *testing.T) {
 	c := &ChannelDestination{}
 	err := c.Deliver(context.Background(), &optimusv1.LogEvent{})
 	must.ErrorIs(t, ErrMissingChannel, err)
+}
+
+func TestDeliver(t *testing.T) {
+	ch := make(chan *optimusv1.LogEvent)
+	cd := &ChannelDestination{
+		Output: ch,
+	}
+	eg, _ := errgroup.WithContext(context.Background())
+	eg.Go(func() error {
+		err := cd.Deliver(context.Background(), &optimusv1.LogEvent{Id: "test"})
+		return err
+	})
+	eg.Go(func() error {
+		data := <-ch
+		must.Eq(t, &optimusv1.LogEvent{Id: "test"}, data)
+		return nil
+	})
+	must.NoError(t, eg.Wait())
 }
