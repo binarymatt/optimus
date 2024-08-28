f9e3ec4e599fa0ae30500915844ec31c59fa5221
Matt George <mgeorge@hashicorp.com>
2024-08-28 16:18:43 -0600

    more transformation tests

--- internal/transformation/transformation_test.go
+++ internal/transformation/transformation_test.go
@@ -2,9 +2,12 @@ package transformation
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/shoenig/test/must"
+	"golang.org/x/sync/errgroup"
 
 	optimusv1 "github.com/binarymatt/optimus/gen/optimus/v1"
 )
@@ -19,4 +22,33 @@ func TestNew(t *testing.T) {
 	}
 	tr = New("test_name", transform)
 	must.NotNil(t, tr.transformer)
+}
+
+func TestProcess_HappyPath(t *testing.T) {
+	transformed := false
+	transform := func(ctx context.Context, evt *optimusv1.LogEvent) (*optimusv1.LogEvent, error) {
+		fmt.Println("transoforming")
+		transformed = true
+		return evt, nil
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	tr := New("test_name", transform)
+	_ = tr.Init()
+	eg := new(errgroup.Group)
+	eg.Go(func() error {
+		return tr.Process(ctx)
+	})
+	evt := &optimusv1.LogEvent{
+		Id: "test",
+	}
+	eg.Go(func() error {
+		tr.Subscriber.Signal(evt)
+		time.Sleep(10 * time.Millisecond)
+		cancel()
+		return nil
+	})
+	err := eg.Wait()
+	must.NoError(t, err)
+	must.True(t, transformed)
+
 }
